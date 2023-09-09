fe912ebf62cdff9986d01d6770013b38e45130d8
yuxuehong <2808723214@qq.com>
2023-09-10 00:23:30 +0800

    airtest自动化测试

--- airtest1.air/airtest1.py
+++ airtest1.air/airtest1.py
@@ -7,10 +7,16 @@ from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
 auto_setup(__file__)
 
-poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+poco = AndroidUiautomationPoco()
 
 app = "com.zhao.myreader"
 
 start_app(app) #启动app
 
 poco(text="书城").click()
+poco("com.zhao.myreader:id/iv_search").click()
+poco("com.zhao.myreader:id/et_search_key").click()
+text("大佬")
+poco("com.zhao.myreader:id/tv_search_conform").click()
+poco("android.widget.FrameLayout").child("android.widget.LinearLayout").offspring("android:id/content").child("android.widget.RelativeLayout").offspring("android.widget.ImageView").click()
+stop_app(app)
